0273d20cb3be9632986a4e847ad11f191de20784
nowell.yang <nowell.yang@ly.com>
2022-06-19 23:01:01 +0800

    feat  rm cache

--- src/runtime-core/h.ts
+++
@@ -1,6 +0,0 @@
-
-import { createVNode } from "./vnode";
-
-export function h(ype, props?, children?) {
-  return createVNode(ype, props, children)
-}
--- src/shared/index.ts
+++
@@ -1,3 +0,0 @@
-export const extend = Object.assign
-
-export const isObject = (val) => val != null && typeof val === 'object'
